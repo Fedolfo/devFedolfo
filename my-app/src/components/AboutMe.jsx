99effd88921186978502e5486ed3e2e18f59f5c1
Filipe <filipecandido123@hotmail.com>
2021-12-21 01:00:48 -0300

    texto sobre mim

--- my-app/src/components/AboutMe.jsx
+++ my-app/src/components/AboutMe.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './css/Body/aboutMe.css';
+import filipe from '../images/filipe.jpeg';
 
 const AboutMe = () => {
   return (
     <article className="container-about-me">
-      <p>É um prazer conhecê-lo(s), me chamo Filipe Candido estudo na <a href="https://www.betrybe.com/">Trybe</a>
+      <img src={ filipe } alt="" width="200px"/>
+      <p>É um prazer conhecê-lo(s), me chamo Filipe Cândido estudo na
+        <a href="https://www.betrybe.com/"><strong> Trybe</strong></a> a 6 meses
+        onde estou abordando sobre estudos de fundamentos de
+        desenvolvimento web, desenvolvimento, <strong>Front-end, Back-end,</strong> ciência da
+        computação, engenharia de software, <strong> metodologias ágeis e habilidades
+        comportamentais.</strong>
       </p>
+      <p>Moro em Minas Gerais, na cidade de Vespasiano e
+        minha idade é de 20 anos.
+      </p>
+      <p>Sintam-se a vontade em dar uma olhada no meu portfólio!</p>
     </article>
   );
 };
