ad277eee2a46db31fdea127f3a659444fa2a64e4
Filipe <filipecandido123@hotmail.com>
2021-12-24 18:28:59 -0300

    adicionado 4 projetos

--- my-app/src/components/Projects.jsx
+++ my-app/src/components/Projects.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Header from "./Header";
 import "./css/project/projects.css";
 import pixelArt from "../images/projects/pixel-art.png";
+import licoesAprendidas from "../images/projects/licoes.png";
+import trybewarts from "../images/projects/trybewarts.png";
+import cartShop from "../images/projects/cartShop.png";
 
 function Projects () {
   return (
@@ -14,7 +17,7 @@ function Projects () {
           <div className="panel-container">
             <div className="control-panel">
                 <ul>
-                <h3>Pixel Art</h3>
+                    <h3>Pixel art</h3>
                   <li>
                     <a href="https://pixel-art-tan.vercel.app/" target="_blank" rel="noreferrer">
                       <img src={ pixelArt } alt="pagina do pixel art"  className="default-container-image"/>
@@ -24,19 +27,34 @@ function Projects () {
             </div>
             <div className="control-panel">
                 <ul>
-                <h3>Pixel Art</h3>
+                    <h3>Lições Aprendidas</h3>
                   <li>
-                    <a href="https://pixel-art-tan.vercel.app/" target="_blank" rel="noreferrer">
-                      <img src={ pixelArt } alt="pagina do pixel art"  className="default-container-image"/>
+                    <a href="https://lessons-learned.vercel.app/" target="_blank" rel="noreferrer">
+                      <img src={ licoesAprendidas } alt="página de lições aprendidas"  className="default-container-image"/>
                     </a>
                   </li>
                 </ul>
             </div>
-            {/* <ul>
-              <li>Venus</li>
-              <li>Earth</li>
-              <li>Mars</li>
-            </ul> */}
+            <div className="control-panel">
+                <ul>
+                    <h3>Trybewarts</h3>
+                  <li>
+                    <a href="https://trybe-warts-khaki.vercel.app/" target="_blank" rel="noreferrer">
+                      <img src={ trybewarts } alt="página de lições aprendidas"  className="default-container-image"/>
+                    </a>
+                  </li>
+                </ul>
+            </div>
+            <div className="control-panel">
+                <ul>
+                    <h3>Carrinho de compras</h3>
+                  <li>
+                    <a href="https://carrinho-compras-gamma.vercel.app/" target="_blank" rel="noreferrer">
+                      <img src={ cartShop } alt="página de lições aprendidas"  className="default-container-image"/>
+                    </a>
+                  </li>
+                </ul>
+            </div>
         </div>
       </section>
     </main>
