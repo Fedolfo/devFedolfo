fdadb6bcb05398afb552678d2e3ffae8b5a37cf1
Filipe <filipecandido123@hotmail.com>
2022-04-21 13:58:47 -0300

    feat: remove photo de perfil no componente sobre mim

--- my-app/src/components/AboutMe.jsx
+++ my-app/src/components/AboutMe.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import './css/Body/aboutMe.css';
-import filipe from '../images/filipe.jpeg';
 
 const AboutMe = () => {
   return (
     <article className="container-about-me">
-        <img
-          src={ filipe }
-          alt="Foto do filipe"
-          width="200px"
-          className="photo-perfil"
-        />
       <div className="container-text">
         <p>É um prazer conhecê-lo(s), me chamo Filipe Cândido estudo na
           <a href="https://www.betrybe.com/"><strong> Trybe</strong></a> a 6 meses
