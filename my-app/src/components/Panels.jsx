1a6d0fdf2689fd9ced15fd51a3eccf2e3b50f3e6
Filipe <filipecandido123@hotmail.com>
2022-04-22 18:51:10 -0300

    refactor: componente Panels

--- my-app/src/components/Panels.jsx
+++ my-app/src/components/Panels.jsx
@@ -1,24 +1,18 @@
-import PropTypes from "prop-types"
-import "./css/project/projects.css";
+import PropTypes from 'prop-types';
+import './css/project/projects.css';
 
-function Panels({ title, onClick, photo, descriptionPhoto }) {
+function Panels({ onClick, photo, descriptionPhoto }) {
   return (
-    <div className="control-panel">
-      <ul onClick={onClick} >
-        <h3>{title}</h3>
-        <li>
-          <img src={photo} alt={descriptionPhoto} className="default-container-image" />
-        </li>
-      </ul>
+    <div className="panel" onClick={onClick}>
+      <img src={photo} alt={descriptionPhoto} />
     </div>
   );
 }
 
 Panels.propTypes = {
   descriptionPhoto: PropTypes.string,
+  onClick: PropTypes.func,
   photo: PropTypes.string,
-  title: PropTypes.string,
-  url: PropTypes.string
 }.isRequired;
 
 export default Panels;
