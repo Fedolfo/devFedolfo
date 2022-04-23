e557dcff33f46ad7b8c56d39b6cd5501636d7e52
Filipe <filipecandido123@hotmail.com>
2022-04-23 01:49:54 -0300

    [feat]: adiciona componente footer

---
+++ my-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/Footer/footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Desenvolvido por Filipe</p>
+    </footer>
+  );
+}
+
+export default Footer;
