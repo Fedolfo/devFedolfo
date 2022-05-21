ad7e2f5496ae6cfad5b4ee2712aeab012eb731ea
Filipe <filipecandido123@hotmail.com>
2022-05-21 17:56:17 -0300

    [refactor]: texto sobre mim

--- my-app/src/components/AboutMe/AboutMe.jsx
+++ my-app/src/components/AboutMe/AboutMe.jsx
@@ -7,20 +7,22 @@ const AboutMe = () => {
       <div className="container-text">
         <p>
           É um prazer conhecê-lo(s), me chamo Filipe Cândido e sou{' '}
-          <strong>Desenvolvedor Web Júnior. </strong>
+          <strong>Desenvolvedor Full Stack Júnior. </strong>
           Sou uma pessoa apaixonado desde a minha infância e adolescência com a
           computação, onde me divertia e me divirto até hoje jogando com os meu
           melhores amigos virtuais como o League of Legends, Valorant e o
           Counter Strike.
         </p>
+        <br />
         <p>
           É agora a computação faz parte de um novo <strong>caminho</strong> pra
-          mim que é seguir <strong>carreira</strong> na área de desenvolvimento
-          web. E nesse caminho tenho sempre um objetivo,{' '}
+          mim que é seguir <strong>carreira</strong> na área de "Engenheiro
+          de Software". E nesse caminho tenho sempre um objetivo,{' '}
           <strong>aprender</strong> e desenvolver novas habilidades, sempre
           procurando novas tecnologias e novas ferramentas para melhorar minhas
           habilidades.
         </p>
+        <br />
         <p>
           Atualmente estou cursando o curso da{' '}
           <a href="https://www.betrybe.com/">
@@ -31,6 +33,7 @@ const AboutMe = () => {
           engenharia de software, metodologias ágeis e habilidades
           comportamentais.
         </p>
+        <br />
         <p>
           É esse portfólio foi criado para mostrar as minhas
           <strong> habilidades</strong> e <strong>experiências</strong> que
